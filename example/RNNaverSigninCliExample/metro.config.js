0883b3709b0b1dd50c095a528bcc85b28775ae8d
dijeungi <dijeungi@naver.com>
2026-04-19 13:43:14 +0900

    chore(example): CLI 예제를 로컬 패키지로 연결 및 로그인 화면 추가

--- example/RNNaverSigninCliExample/metro.config.js
+++ example/RNNaverSigninCliExample/metro.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const packageRoot = path.resolve(__dirname, '..', '..');
 
 /**
  * Metro configuration
@@ -6,6 +9,12 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [packageRoot],
+  resolver: {
+    disableHierarchicalLookup: true,
+    nodeModulesPaths: [path.resolve(__dirname, 'node_modules')],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
